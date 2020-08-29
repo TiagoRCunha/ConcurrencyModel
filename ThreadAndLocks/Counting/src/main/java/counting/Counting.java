2d5d1b3e7bf9baa4482015df700818bec1c0770c
Tiago Rodrigues Cunha <tiago.r.c.sn@gmail.com>
2020-08-29 15:36:37 -0300

    counting exercise but better

--- ThreadAndLocks/Counting/src/main/java/counting/Counting.java
+++ ThreadAndLocks/Counting/src/main/java/counting/Counting.java
@@ -1,31 +1,27 @@
 package counting;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Counting {
   public static void main(String[] args) throws InterruptedException {
-    class Counter {
-      private int count = 0;
 
-      public synchronized void increment() {
-        ++count;
-      }
+    final AtomicInteger counter = new AtomicInteger();
 
-      public int getCount() {
-        return count;
-      }
-    }
-    final Counter counter = new Counter();
     class CountingThread extends Thread {
       public void run() {
         for (int x = 0; x < 10000; ++x)
-          counter.increment();
+          counter.incrementAndGet();
       }
     }
+
     CountingThread t1 = new CountingThread();
     CountingThread t2 = new CountingThread();
+
     t1.start();
     t2.start();
     t1.join();
     t2.join();
-    System.out.println(counter.getCount());
+
+    System.out.println(counter.get());
   }
 }

ed9d733683fdfecbdcdaee7ab3ee83a6b7678335
Tiago Rodrigues Cunha <tiago.r.c.sn@gmail.com>
2020-08-29 14:23:37 -0300

    downloader exercise fixed

---
+++ ThreadAndLocks/Downloader/src/main/java/downloader/HttpDownload.java
@@ -0,0 +1,19 @@
+package downloader;
+
+import java.net.URL;
+
+public class HttpDownload {
+
+	public static void main(String[] args) throws Exception {
+		URL from = new URL("http://download.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles.xml.bz2");
+		Downloader downloader = new Downloader(from, "download.out");
+		downloader.start();
+		downloader.addListener(new ProgressListener() {
+			public void onProgress(int n) {
+				System.out.print("\r" + n);
+				System.out.flush();
+			}
+		});
+		downloader.join();
+	}
+}
